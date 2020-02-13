1814a59948dd997c03e76348239f9ce7d1e4cb82
tlozano <tlozano@bishopfox.com>
2020-02-13 15:56:53 -0700

    Added scripts

---
+++ Import-Aws-Credentials-Csv.ps1
@@ -0,0 +1,10 @@
+﻿# Imports aws credentials into your environment variables
+Function Import-Aws-Credentials-Csv {
+    [cmdletbinding()]
+    Param( [string] $credentialsCsvPath)
+Process {
+    $creds = Import-Csv $credentialsCsvPath
+    $Env:AWS_ACCESS_KEY_ID = $creds."Access key ID"
+    $Env:AWS_SECRET_ACCESS_KEY = $creds."Secret access key"
+    }
+}
---
+++ Revoke-All-Default-Sg-Rules.ps1
@@ -0,0 +1,6 @@
+﻿$regions = (aws ec2 describe-regions | jq .[][].RegionName)
+foreach ($region in $regions) {
+    $id = (aws ec2 describe-security-groups --group-name Default --region $region| jq .SecurityGroups[].GroupId)
+    aws ec2 revoke-security-group-ingress --group-id $id --source-group $id --protocol all --region $region
+    aws ec2 revoke-security-group-egress --group-id $id --cidr 0.0.0.0/0 --protocol all --region $region
+    }
